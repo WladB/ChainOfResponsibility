090224e2203ca55cea62ac6c41ad778665368ddf
Vlad <wbcom9543@gmail.com>
2024-02-18 15:18:44 +0200

    first commit

---
+++ Chain_of_Responsibility/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chain_of_Responsibility
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var bot = new Bot();
+            var operat = new Operator();
+            var geek = new geek();
+
+            bot.SetNext(operat).SetNext(geek);
+            if (bot.Handle(textBox2.Text) != null)
+            {
+                textBox1.Text = bot.Handle(textBox2.Text).ToString();
+            }
+            else {
+                MessageBox.Show("У тех підтримці вас нерозуміють");
+                textBox2.Text = "";
+            }
+        }
+    }
+}
---
+++ Chain_of_Responsibility/Handler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_of_Responsibility
+{
+  public class BaseHandler
+    {
+        protected BaseHandler next;
+        public virtual object Handle(string request) {
+            if (next != null)
+            {
+                return this.next.Handle(request);
+            }
+            else {
+                return null;
+            }
+        }
+        public BaseHandler SetNext(BaseHandler h) {
+            this.next = h;
+            return h;
+        }
+    }
+    public class Bot : BaseHandler {
+        public override object Handle(string request)
+        {
+            if (request.Contains("у мене не працює"))
+            {
+                return $"Спробуйте вимкнути та увімкнути знову { request.Substring(request.IndexOf("у мене не працює") + 17)}";
+      
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+   
+    }
+    public class Operator : BaseHandler
+    {
+        public override object Handle(string request)
+        {
+            if (request.Contains("мені не допомогло"))
+            {
+                return $"Спробуйте ще раз { request.Substring(request.LastIndexOf("мені не допомогло") + 17)}";
+
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+
+    }
+    public class geek : BaseHandler
+    {
+        public override object Handle(string request)
+        {
+            if (request.Contains("мені це також не допомогло"))
+            {
+                return $"Спробуйте щось окрім цього: { request.Substring(request.LastIndexOf("мені це також не допомогло") + 27)}";
+
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+
+    }
+}
